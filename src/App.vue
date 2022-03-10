4637e6815dbc859970c4788f23d3856967483135
Quang Truong <nqtruong2510@gmail.com>
2022-03-10 23:08:01 +0700

    Ui

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <loginForm> 
+    </loginForm>
+  </div>
+  
+
+</template>
+
+<script>
+import loginForm from './components/loginForm.vue';
+export default {
+  name: 'App',
+  components: {
+    loginForm,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
